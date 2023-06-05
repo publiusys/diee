9dad2e17328147ed090abd7b7f84f7ce5fa31916
hand32 <hand32@server.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-06-05 14:17:56 -0600

    add msr setup note

--- msr_setup.sh
+++ msr_setup.sh
@@ -7,5 +7,7 @@ sudo chgrp msr /dev/cpu/*/msr
 sudo ls -l /dev/cpu/*/msr
 sudo chmod g+rw /dev/cpu/*/msr
 sudo usermod -aG msr hand32
-sudo newgrp msr
+
+echo "Re-login for msr group changes to take effect"
+#sudo newgrp msr
 
