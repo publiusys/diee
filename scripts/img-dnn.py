bc3795477f0cd752915108b285904a7c6fd94b9c
hand32 <hand32@harness.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-06-05 15:34:38 -0600

    update img-dnn scripts

--- scripts/img-dnn.py
+++ scripts/img-dnn.py
@@ -44,8 +44,6 @@ def runLocalCommand(com):
     #p1 = Popen(list(filter(None, com.strip().split(' '))), stdout=PIPE, stderr=PIPE)
     return p1
 
-#TBENCH_QPS=1000 ./img-dnn/img-dnn_client_networked
-#TBENCH_WARMUPREQS=5000 TBENCH_MAXREQS=20000 TBENCH_NCLIENTS=1 ./img-dnn/img-dnn_server_networked -r 10 -f /data/tailbench.inputs/img-dnn/models/model.xml -n 100000000
 def run():
     global TBENCH_QPS
     global TBENCH_WARMUPREQS
@@ -56,7 +54,7 @@ def run():
     serverOut = runRemoteCommand(f"TBENCH_WARMUPREQS={TBENCH_WARMUPREQS} TBENCH_MAXREQS={TBENCH_MAXREQS} TBENCH_NCLIENTS={TBENCH_NCLIENTS} ~/bayop/tailbench/img-dnn/img-dnn_server_networked -r 10 -f /data/tailbench.inputs/img-dnn/models/model.xml -n 100000000", TBENCH_SERVER)
     time.sleep(5)
 
-    clientOut = runLocalCommand(f"TBENCH_QPS=1000 ~/bayop/tailbench/img-dnn/img-dnn_client_networked")
+    clientOut = runLocalCommand(f"TBENCH_QPS={TBENCH_QPS} ~/bayop/tailbench/img-dnn/img-dnn_client_networked")
     print("Client Output:")
     for out in clientOut.communicate():
         print(str(out.strip()), end=" ")
@@ -68,5 +66,22 @@ def run():
     print("")
     
 if __name__ == '__main__':
-    #print(f"TBENCH_WARMUPREQS={TBENCH_WARMUPREQS} TBENCH_MAXREQS={TBENCH_MAXREQS} TBENCH_NCLIENTS={TBENCH_NCLIENTS} TBENCH_QPS={TBENCH_QPS} TBENCH_SERVER={TBENCH_SERVER}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--qps", help="QPS")
+    parser.add_argument("--warmup", help="Warmup requests")
+    parser.add_argument("--maxq", help="Max requests")
+    parser.add_argument("--nclients", help="Number of clients")
+
+    args = parser.parse_args()
+    numTrials = 30
+    if args.qps:
+        TBENCH_QPS = int(args.qps)
+    if args.warmup:
+        TBENCH_WARMUPREQS = int(args.warmup)
+    if args.maxq:
+        TBENCH_MAXREQS = int(args.maxq)
+    if args.nclients:
+        TBENCH_NCLIENTS = int(args.nclients)
+    print(f"TBENCH_WARMUPREQS={TBENCH_WARMUPREQS} TBENCH_MAXREQS={TBENCH_MAXREQS} TBENCH_NCLIENTS={TBENCH_NCLIENTS} TBENCH_QPS={TBENCH_QPS} TBENCH_SERVER={TBENCH_SERVER}")
+    
     run()
--- scripts/run_img-dnn.sh
+++ scripts/run_img-dnn.sh
@@ -3,20 +3,28 @@
 currdate=`date +%m_%d_%Y_%H_%M_%S`
 set -x
 
-## start power logging
-ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
-ssh ${TBENCH_SERVER} sudo rm /data/rapl_log.log
-ssh ${TBENCH_SERVER} sudo systemctl restart rapl_log
+export MQPS=${MQPS:-"1000"}
+export NITERS=${NITERS:='1'}
 
-python img-dnn.py
+for qps in ${MQPS}; do
+    for i in `seq 0 1 $NITERS`; do
+	## start power logging
+	ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
+	ssh ${TBENCH_SERVER} sudo rm /data/rapl_log.log
+	ssh ${TBENCH_SERVER} sudo systemctl restart rapl_log
 
-## stop power logging
-ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
-#ssh ${TBENCH_SERVER} cat /data/rapl_log.log
+	python img-dnn.py --qps ${qps}
 
-## retrieve logs
-scp -r ${TBENCH_SERVER}:/data/rapl_log.log server_rapl_log.log
-python ~/bayop/tailbench/utilities/parselats.py lats.bin
-rm lats.txt
-rm lats.bin 
-tail -n 10 server_rapl_log.log
+	## stop power logging
+	ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
+	#ssh ${TBENCH_SERVER} cat /data/rapl_log.log
+
+	## retrieve logs
+	scp -r ${TBENCH_SERVER}:/data/rapl_log.log server_rapl_log.log
+	python ~/bayop/tailbench/utilities/parselats.py lats.bin
+	rm lats.txt
+	rm lats.bin 
+	tail -n 10 server_rapl_log.log
+	echo "###################################"
+    done
+done
