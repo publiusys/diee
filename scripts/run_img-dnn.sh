0dec8816cff06d9c710e3c4b9855f267ae0eb484
hand32 <hand32@harness.hand32-158637.bayopsys-pg0.utah.cloudlab.us>
2023-06-06 13:04:22 -0600

    update run_img-dnn.sh

--- scripts/run_img-dnn.sh
+++ scripts/run_img-dnn.sh
@@ -1,30 +1,51 @@
 #! /bin/bash
 
 currdate=`date +%m_%d_%Y_%H_%M_%S`
-set -x
+#set -x
 
 export MQPS=${MQPS:-"1000"}
 export NITERS=${NITERS:='1'}
+export WARMUP=${WARMUP:-"5000"}
+export MAXQ=${MAXQ:-"20000"}
 
-for qps in ${MQPS}; do
-    for i in `seq 0 1 $NITERS`; do
-	## start power logging
-	ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
-	ssh ${TBENCH_SERVER} sudo rm /data/rapl_log.log
-	ssh ${TBENCH_SERVER} sudo systemctl restart rapl_log
+function runLinuxDefault
+{
+    echo "********************** runLinuxDefault **********************"
+    echo "mkdir ${currdate}"
+    mkdir ${currdate}
 
-	python img-dnn.py --qps ${qps}
+    for qps in ${MQPS}; do
+	for i in `seq 0 1 $NITERS`; do
+	    echo "### ${i} python img-dnn.py --qps ${qps} --warmup ${WARMUP} --maxq ${MAXQ} ###"
+	    echo ""
+	    
+	    ## start power logging
+	    ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
+	    ssh ${TBENCH_SERVER} sudo rm /data/rapl_log.log
+	    ssh ${TBENCH_SERVER} sudo systemctl restart rapl_log
 
-	## stop power logging
-	ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
-	#ssh ${TBENCH_SERVER} cat /data/rapl_log.log
+	    python img-dnn.py --qps ${qps} --warmup ${WARMUP} --maxq ${MAXQ}
 
-	## retrieve logs
-	scp -r ${TBENCH_SERVER}:/data/rapl_log.log server_rapl_log.log
-	python ~/bayop/tailbench/utilities/parselats.py lats.bin
-	rm lats.txt
-	rm lats.bin 
-	tail -n 10 server_rapl_log.log
-	echo "###################################"
+	    ## stop power logging
+	    ssh ${TBENCH_SERVER} sudo systemctl stop rapl_log
+	    #ssh ${TBENCH_SERVER} cat /data/rapl_log.log
+
+	    ## retrieve logs
+	    scp -r ${TBENCH_SERVER}:/data/rapl_log.log rapl_${i}_${qps}.log
+	    python ~/bayop/tailbench/utilities/parselats.py lats.bin > lats_${i}_${qps}.log
+	    mv lats.bin lats_${i}_${qps}.bin
+	    rm lats.txt
+
+	    mv rapl_${i}_${qps}.log ${currdate}
+	    mv lats_${i}_${qps}.log ${currdate}
+	    mv lats_${i}_${qps}.bin ${currdate}
+	    
+	    #tail -n 10 server_rapl_log.log
+	    echo "########################################################"
+	    echo ""
+	done
     done
-done
+    echo "*********** ${currdate} FINISHED *************"
+}
+
+"$@"
