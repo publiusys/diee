5c4bde4de9d4fd730ef6730dd53895a851e0e9ce
hand32 <hand32@harness.hand32-160926.bayopsys-pg0.cloudlab.umass.edu>
2023-07-03 17:53:46 -0400

    xapian rs620

---
+++ scripts/rs620/xapian/run_bayesopt.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#set -x
+
+export NTRIALS=${NTRIALS:-"60"}
+export PERCENTILE=${PERCENTILE:-"90"}
+export LATENCY=${LATENCY:-"5000"}
+export MQPS=${MQPS:-"1500"}
+
+function run() {
+    for qps in ${MQPS}; do
+	for percentile in ${PERCENTILE}; do
+	    for latency in ${LATENCY}; do
+		newdir="bayesopt_trials${NTRIALS}_qps${qps}_percentile${percentile}_latency${latency}"
+		echo $newdir
+		mkdir ${newdir}
+		python -u bayesopt.py --trials ${NTRIALS} --qps ${qps} --percentile ${percentile} --latency ${latency} > ${newdir}/"${newdir}.log"
+		mv *.bin ${newdir}
+		mv *.log ${newdir}
+		mv *.txt ${newdir}
+		
+		rsync --mkpath -avz ${newdir}/* don:/home/handong/cloudlab/rs620/xapian/linux_static/${newdir}/
+	    done
+	done
+    done
+}
+
+function runMult() {
+    NTRIALS=40 MQPS=1000 PERCENTILE=99 LATENCY=10000 run
+    NTRIALS=40 MQPS=600 PERCENTILE=99 LATENCY=10000 run
+    NTRIALS=40 MQPS=400 PERCENTILE=99 LATENCY=10000 run
+
+    NTRIALS=40 MQPS=1000 PERCENTILE=90 LATENCY=5000 run
+    NTRIALS=40 MQPS=600 PERCENTILE=90 LATENCY=5000 run
+    NTRIALS=40 MQPS=400 PERCENTILE=90 LATENCY=5000 run
+}
+
+$@
+
+# bayesopt_trials60_qps1500_percentile90_latency5000
+# bayesopt_trials60_qps2000_percentile99_latency5000
+
+#bayesopt_trials60_qps2500_percentile99_latency5000
+
+# bayesopt_trials60_qps3000_percentile99_latency5000
