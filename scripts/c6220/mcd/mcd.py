f43a11954fb072f4bf86f974fe6077c381dd3a53
hand32 <hand32@node0.hand32-165655.bayopsys-pg0.apt.emulab.net>
2023-09-07 13:32:18 -0600

    refactor mcd server and agents

--- scripts/c6220/mcd/mcd.py
+++ scripts/c6220/mcd/mcd.py
@@ -12,7 +12,8 @@ import itertools
 import argparse
 import shutil
 
-TBENCH_SERVER = "192.168.1.20"
+TBENCH_SERVER = ""
+TBENCH_AGENTS = []
 TARGET_QPS = 50000
 GITR = ""
 GDVFS = ""
@@ -46,28 +47,27 @@ def runLocalCommand(com):
     return p1
 
 def setITR():
-    global GITR
+    global GITR, TBENCH_SERVER
     p1 = runRemoteCommand(f"ethtool -C enp3s0f0 rx-usecs {GITR}", TBENCH_SERVER)
     p1.communicate()
 
 def setDVFS():
-    global GDVFS
+    global GDVFS, TBENCH_SERVER
     s = "0x10000"+GDVFS
     p1 = runRemoteCommand(f"wrmsr -a 0x199 {s}", TBENCH_SERVER)
     p1.communicate()
 
 def run():
     global TARGET_QPS
-    
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.1")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.2")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.3")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.4")
+    global TBENCH_SERVER
+    global TBENCH_AGENTS
+
+    for agent in TBENCH_AGENTS:        
+        runRemoteCommandGet("pkill -f mutilate", agent)
     time.sleep(1)
-    
-    runRemoteCommands("~/mutilate/mutilate --agentmode --threads=16", "192.168.1.2")
-    runRemoteCommands("~/mutilate/mutilate --agentmode --threads=16", "192.168.1.3")
-    runRemoteCommands("~/mutilate/mutilate --agentmode --threads=16", "192.168.1.4")
+
+    for agent in TBENCH_AGENTS[1:]:
+        runRemoteCommands("~/mutilate/mutilate --agentmode --threads=16", agent)
     time.sleep(1)
     print("pkill mutilate done")
     
@@ -78,11 +78,11 @@ def run():
         print(f"rerunning mcd taskset -c 0-15 ~/memcached/memcached -u nobody -t 16 -m 32G -c 8192 -b 8192 -l {TBENCH_SERVER} -B binary")
         runRemoteCommands(f"taskset -c 0-15 ~/memcached/memcached -u nobody -t 16 -m 32G -c 8192 -b 8192 -l {TBENCH_SERVER} -B binary", TBENCH_SERVER)
         time.sleep(1)
-        runRemoteCommands(f"taskset -c 0 ~/mutilate/mutilate --binary -s {TBENCH_SERVER} --loadonly -K fb_key -V fb_value", "192.168.1.1")
+        runRemoteCommands(f"taskset -c 0 ~/mutilate/mutilate --binary -s {TBENCH_SERVER} --loadonly -K fb_key -V fb_value", TBENCH_AGENTS[0])
         time.sleep(1)
 
     ## run mutilate
-    serverOut = runRemoteCommand(f"taskset -c 0 ~/mutilate/mutilate --binary -s {TBENCH_SERVER} --noload --agent=192.168.1.2,192.168.1.3,192.168.1.4 --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_depth=1 --connections=16 --measure_connections=32 --measure_qps=2000 --qps={TARGET_QPS} --time=30", "192.168.1.1")
+    serverOut = runRemoteCommand(f"taskset -c 0 ~/mutilate/mutilate --binary -s {TBENCH_SERVER} --noload --agent={TBENCH_AGENTS[1:].join(',')} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=4 --measure_depth=1 --connections=16 --measure_connections=32 --measure_qps=2000 --qps={TARGET_QPS} --time=30", TBENCH_AGENTS[0])
 
     print("Server Output:")
     f = open(f"mutilate.out", "w")
@@ -91,18 +91,18 @@ def run():
             f.write(line.strip()+"\n")
     f.close()
 
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.1")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.2")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.3")
-    runRemoteCommandGet("pkill -f mutilate", "192.168.1.4")
-    time.sleep(1)
+    for agent in TBENCH_AGENTS:        
+        runRemoteCommandGet("pkill -f mutilate", agent)
+    time.sleep(1)        
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--qps", help="QPS")
     parser.add_argument("--itr", help="2, 4, etc")
     parser.add_argument("--dvfs", help="0c00 ... 1800")    
-
+    parser.add_argument("--server", help="single server ip addresses")
+    parser.add_argument("--agents", help="comma delimited agent ip addresses")
+    
     args = parser.parse_args()
     if args.qps:
         TARGET_QPS = int(args.qps)
@@ -112,7 +112,18 @@ if __name__ == '__main__':
     if args.dvfs:
         GDVFS = args.dvfs
         setDVFS()
-        
-    print(f"TARGET_QPS={TARGET_QPS} TBENCH_SERVER={TBENCH_SERVER} GITR={GITR} GDVFS={GDVFS}")
+    if args.server:
+        TBENCH_SERVER = args.server
+    else:
+        print(f"Need valid --server: {args.server}")
+        exit()
+    
+    if args.agents:
+        TBENCH_AGENTS = (args.agents).strip().split(',')
+    else:
+        print(f"Need valid --agents: {args.agents}")
+        exit()
+            
+    print(f"TARGET_QPS={TARGET_QPS} TBENCH_SERVER={TBENCH_SERVER} TBENCH_AGENTS={TBENCH_AGENTS} GITR={GITR} GDVFS={GDVFS}")
     
     run()
